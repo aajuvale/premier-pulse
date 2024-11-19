54b5faf52f5f1194647ffc78833645d82fd6758a
aajuvale <ahmed.juvale@gmail.com>
2024-11-18 17:16:21 -0700

    feat: added text input

--- Premier Pulse/ContentView.swift
+++ Premier Pulse/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput: String = "" // State variable to hold user input
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, Ahmed!")
+        VStack(spacing: 20) {
+            Text("What Release Would You Like to Follow?:")
+                .font(.headline)
+            
+            TextField("Type a movie/show/video game title...", text: $userInput)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+
+            Text("You entered: \(userInput)")
+                .font(.subheadline)
         }
         .padding()
     }
