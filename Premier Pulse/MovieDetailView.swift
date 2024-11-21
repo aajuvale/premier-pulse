78f0aa177be0772908b75b92d45f029891c64230
aajuvale <ahmed.juvale@gmail.com>
2024-11-21 16:50:05 -0700

    style: fixed text overflow on synopsis and modified logo sizes

--- Premier Pulse/MovieDetailView.swift
+++ Premier Pulse/MovieDetailView.swift
@@ -13,7 +13,8 @@ struct MovieDetailView: View {
     
     @State private var logoURL: URL?
     @State private var additionalImages: [URL] = []
-    
+    @State private var videos: [Video] = []
+
     var body: some View {
         ZStack {
             // Poster as Background
@@ -37,7 +38,6 @@ struct MovieDetailView: View {
                 Color.black.edgesIgnoringSafeArea(.all)
             }
             
-            // Foreground Content
             ScrollView {
                 VStack(spacing: 20) {
                     // Movie Poster
@@ -72,14 +72,14 @@ struct MovieDetailView: View {
                                 switch phase {
                                 case .empty:
                                     ProgressView()
-                                        .frame(width: 120, height: 60)
+                                        .frame(width: 160, height: 70)
                                         .cornerRadius(10)
                                 case .success(let image):
                                     image.resizable()
                                         .scaledToFit()
-                                        .frame(width: 120, height: 60) // Slightly larger
+                                        .frame(width: 160, height: 70)
                                         .cornerRadius(10)
-                                        .padding(.leading, 16) // Add padding from the left
+                                        .padding(.leading, 16)
                                 case .failure:
                                     Text("No Logo")
                                         .foregroundColor(.gray)
@@ -90,7 +90,10 @@ struct MovieDetailView: View {
                             }
                         }
                         
-                        Spacer()
+//                        Spacer()
+                        Rectangle()
+                                .frame(maxWidth: 60) // Acts as a flexible spacer
+                                .foregroundColor(.clear) // Make it invisible
                         
                         // Track Release Button
                         Button(action: {
@@ -108,7 +111,7 @@ struct MovieDetailView: View {
                             .foregroundColor(.white)
                             .cornerRadius(10)
                         }
-                        .padding(.trailing, 16) // Add padding from the right
+                        .padding(.trailing, 16)
                     }
                     .padding(.horizontal)
                     .frame(maxWidth: .infinity)
@@ -125,32 +128,173 @@ struct MovieDetailView: View {
                     
                     // Movie Overview
                     Text(movie.overview)
+                        .frame(maxWidth: UIScreen.main.bounds.width - 40)
                         .font(.body)
                         .padding(.horizontal)
                         .multilineTextAlignment(.center)
                         .foregroundColor(.white)
                         .shadow(radius: 5)
+                        
+                   
+                    
+                    Divider()
+                    
+                    // Additional Images Section
+                    if !additionalImages.isEmpty {
+                        Text("Additional Images")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.top)
+
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(additionalImages, id: \.self) { imageURL in
+                                    AsyncImage(url: imageURL) { phase in
+                                        switch phase {
+                                        case .empty:
+                                            ProgressView()
+                                                .frame(width: 150, height: 100)
+                                                .cornerRadius(10)
+                                        case .success(let image):
+                                            image.resizable()
+                                                .frame(width: 150, height: 100)
+                                                .cornerRadius(10)
+                                                .scaledToFit()
+                                        case .failure:
+                                            Image(systemName: "photo")
+                                                .resizable()
+                                                .frame(width: 150, height: 100)
+                                                .cornerRadius(10)
+                                                .foregroundColor(.gray)
+                                        @unknown default:
+                                            EmptyView()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    
+                    // Videos Section
+                    if !videos.isEmpty {
+                        Text("Videos")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .padding(.top)
+
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(videos, id: \.id) { video in
+                                    Button(action: {
+                                        if let youtubeURL = URL(string: "https://www.youtube.com/watch?v=\(video.key)") {
+                                            UIApplication.shared.open(youtubeURL)
+                                        }
+                                    }) {
+                                        VStack {
+                                            Image(systemName: "play.rectangle.fill")
+                                                .resizable()
+                                                .frame(width: 150, height: 100)
+                                                .foregroundColor(.red)
+                                            Text(video.name)
+                                                .font(.caption)
+                                                .foregroundColor(.white)
+                                                .multilineTextAlignment(.center)
+                                                .frame(width: 150)
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
                 .padding()
             }
         }
-        .navigationTitle(movie.title)
+        .navigationBarTitle("", displayMode: .inline)
         .onAppear {
             fetchMovieImages()
+            fetchVideos()
         }
     }
     
     // Fetch movie images from TMDB API
     func fetchMovieImages() {
+        guard let apiKey = Utilities.getAPIKey() else {
+            print("Invalid API key")
+            return
+        }
+
+        // Fetch logos
+        if let logoURL = URL(string: "https://api.themoviedb.org/3/movie/\(movie.id)/images?api_key=\(apiKey)&include_image_language=en") {
+            let logoTask = URLSession.shared.dataTask(with: logoURL) { data, response, error in
+                if let error = error {
+                    print("Failed to fetch logos: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let data = data else {
+                    print("No data received for logos")
+                    return
+                }
+
+                do {
+                    let decodedResponse = try JSONDecoder().decode(MovieImagesResponse.self, from: data)
+
+                    DispatchQueue.main.async {
+                        // Get the first English logo
+                        self.logoURL = decodedResponse.logos
+                            .first
+                            .flatMap { URL(string: "https://image.tmdb.org/t/p/w500\($0.filePath)") }
+                    }
+                } catch {
+                    print("Failed to decode logos response: \(error.localizedDescription)")
+                }
+            }
+            logoTask.resume()
+        }
+
+        // Fetch additional images (backdrops)
+        if let backdropsURL = URL(string: "https://api.themoviedb.org/3/movie/\(movie.id)/images?api_key=\(apiKey)") {
+            let backdropsTask = URLSession.shared.dataTask(with: backdropsURL) { data, response, error in
+                if let error = error {
+                    print("Failed to fetch additional images: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let data = data else {
+                    print("No data received for additional images")
+                    return
+                }
+
+                do {
+                    let decodedResponse = try JSONDecoder().decode(MovieImagesResponse.self, from: data)
+
+                    DispatchQueue.main.async {
+                        // Fetch backdrops
+                        self.additionalImages = decodedResponse.backdrops.map {
+                            URL(string: "https://image.tmdb.org/t/p/w500\($0.filePath)")
+                        }.compactMap { $0 }
+                    }
+                } catch {
+                    print("Failed to decode additional images response: \(error.localizedDescription)")
+                }
+            }
+            backdropsTask.resume()
+        }
+    }
+
+
+    // Fetch videos from TMDB API
+    func fetchVideos() {
         guard let apiKey = Utilities.getAPIKey(),
-              let url = URL(string: "https://api.themoviedb.org/3/movie/\(movie.id)/images?api_key=\(apiKey)&include_image_language=en") else {
+              let url = URL(string: "https://api.themoviedb.org/3/movie/\(movie.id)/videos?api_key=\(apiKey)&language=en-US") else {
             print("Invalid API key or URL")
             return
         }
 
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
-                print("Failed to fetch images: \(error.localizedDescription)")
+                print("Failed to fetch videos: \(error.localizedDescription)")
                 return
             }
 
@@ -159,36 +303,32 @@ struct MovieDetailView: View {
                 return
             }
 
-            // Debug: Print the raw JSON response
-            if let jsonString = String(data: data, encoding: .utf8) {
-                print("Raw JSON response: \(jsonString)")
-            }
-
             do {
-                let decodedResponse = try JSONDecoder().decode(MovieImagesResponse.self, from: data)
+                let decodedResponse = try JSONDecoder().decode(VideoResponse.self, from: data)
 
                 DispatchQueue.main.async {
-                    // Use the first logo (should already be filtered by the query parameter)
-                    self.logoURL = decodedResponse.logos
-                        .first
-                        .flatMap { URL(string: "https://image.tmdb.org/t/p/w500\($0.filePath)") }
-
-                    // Map additional images
-                    self.additionalImages = decodedResponse.backdrops.map {
-                        URL(string: "https://image.tmdb.org/t/p/w500\($0.filePath)")
-                    }.compactMap { $0 }
+                    self.videos = decodedResponse.results
                 }
             } catch {
-                print("Failed to decode images response: \(error.localizedDescription)")
+                print("Failed to decode videos response: \(error.localizedDescription)")
             }
         }
 
         task.resume()
     }
+}
 
+// Models for Videos API Response
+struct VideoResponse: Codable {
+    let results: [Video]
+}
 
-
-    
+struct Video: Codable {
+    let id: String
+    let key: String
+    let name: String
+    let site: String
+    let type: String
 }
 
 // Models for Movie Images API Response
